df1be2b230d61a4bdb5a503c01b5ce676e12b027
OUALI42 <ouali.tiguemounine@edu.esiee-it.fr>
2025-02-04 10:02:03 +0100

    Ouali
    Ajout d'une barre de stamina pour l'attaque special + systeme d'attaque + reglages de bug.

---
+++ Assets/Script/StaminaBar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxStamina(int Stamina)
+    {
+        slider.maxValue = Stamina;
+        slider.value = Stamina;
+    }
+
+    public void SetStamina(int stamina)
+{
+    slider.value = stamina; // Assure que le slider est bien mis à jour
+    Debug.Log($"Endurance mise à jour sur la barre : {stamina}");
+}
+
+}
