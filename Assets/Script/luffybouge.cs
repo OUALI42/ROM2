dbbd1cc6642027274326a9642cac74987070fa7c
OUALI42 <ouali.tiguemounine@edu.esiee-it.fr>
2025-03-01 11:25:41 +0100

    Ouali
    Initial commit

--- Assets/Script/luffybouge.cs
+++ Assets/Script/luffybouge.cs
@@ -37,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
             isGrounded = false;
             animator.SetBool("isJumping", !isGrounded);
+            animator.Play("Movement");
         }
     }
 
